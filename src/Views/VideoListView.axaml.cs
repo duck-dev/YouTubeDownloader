32602665ddcbd8b8632649dff0ecf92d1ee9435f
Duck <61479436+duck-dev@users.noreply.github.com>
2024-02-03 20:36:58 +0100

    Add basic layout to `MainWindow` and create relevant views
    - Created two new views:
        - `VideoListView`: Where the user can search for videos and those that match the keyword are listed there.
        - `QueueView`: Where videos are queued for download.
    - Created a basic layout to place those two views into the main window.

---
+++ src/Views/VideoListView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace YouTubeDownloader.Views;
+
+public partial class VideoListView : UserControl
+{
+    public VideoListView()
+    {
+        InitializeComponent();
+    }
+}
