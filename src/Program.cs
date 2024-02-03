b4181160c63f031d8d25a5409cddbecfd9ad250c
Duck <61479436+duck-dev@users.noreply.github.com>
2024-02-03 20:45:21 +0100

    Get YouTube-API-key from project arguments
    - Get and save the YouTube-API-key from the project arguments.

--- src/Program.cs
+++ src/Program.cs
@@ -6,12 +6,17 @@ namespace YouTubeDownloader;
 
 internal static class Program
 {
+    internal static string ApiKey { get; private set; } = string.Empty;
+    
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        ApiKey = args[0];
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
